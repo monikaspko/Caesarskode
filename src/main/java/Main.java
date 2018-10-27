36cfc3f8cbd7f269cc924a62142fcb8254bbb3dd
mko <mko@computas.com>
2018-10-27 20:01:30 +0200

    fjernet kommentar

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,7 @@ package main.java;
 import java.util.List;
 
 /**
- * Krypterer og dekrypterer med alfabetet deklarert i FileReader, dvs. alle bokstaver i det norske alfabetet, både store og små.
- * Linjeskift og mellomrom (+ andre symboler) blir ikke endret!
- *
- * Svaret skrives til fil.
- *
- * Forventet argumenter:
- * 1: path til fil med tekst som skal krypteres/dekrypteres
- * 2: antall forskyvninger i alfabetet (som tall)
- * 3: krypter eller dekrypter
- *
+ * Se readme-fil
  */
 public class Main {
 
